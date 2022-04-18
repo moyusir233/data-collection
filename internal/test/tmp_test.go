320cec32a16c00e64e084e7d179836267a331366
moyu <279972161@qq.com>
2022-04-18 17:29:40 +0800

    增加了对grpc最大连接时间的测试

--- internal/test/tmp_test.go
+++ internal/test/tmp_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"context"
+	v1 "gitee.com/moyusir/data-collection/api/dataCollection/v1"
 	util "gitee.com/moyusir/util/api/util/v1"
 	"reflect"
 	"sync"
@@ -59,5 +60,86 @@ func BenchmarkReflect(b *testing.B) {
 		for _, f := range fields {
 			value.FieldByName(f).Float()
 		}
+	}
+}
+
+func TestMaxConnectionTime(t *testing.T) {
+	// 初始化测试所需环境变量
+	envs := map[string]string{
+		"USERNAME": "init_bucket",
+	}
+	bootstrap, err := generalInit("", envs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	configClient, _, warningDetectClient := StartDataCollectionTestServer(t, bootstrap)
+
+	wg := new(sync.WaitGroup)
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	once := new(sync.Once)
+	cancel := func() {
+		once.Do(func() {
+			cancelFunc()
+		})
+	}
+
+	timeout := time.After(3 * time.Minute)
+	t.Cleanup(func() {
+		cancel()
+		wg.Wait()
+	})
+
+	configUpdateStream, err := configClient.CreateConfigUpdateStream0(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = configUpdateStream.Header()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+
+		defer configUpdateStream.Send(&v1.ConfigUpdateReply{
+			Success: true,
+			End:     true,
+		})
+		go func() {
+			for {
+				_, err := configUpdateStream.Recv()
+				if err != nil {
+					select {
+					case <-ctx.Done():
+					default:
+						t.Error(err)
+						cancel()
+					}
+					return
+				}
+			}
+		}()
+		<-ctx.Done()
+	}()
+
+	configSaveStream, err := configClient.CreateInitialConfigSaveStream0(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer configSaveStream.CloseSend()
+
+	stateInfoSaveStream, err := warningDetectClient.CreateStateInfoSaveStream0(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer stateInfoSaveStream.CloseSend()
+
+	select {
+	case <-timeout:
+		return
+	case <-ctx.Done():
+		t.Fatal("连接被提前断开了")
 	}
 }
